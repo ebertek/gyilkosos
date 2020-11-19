2f990b38debcfd3a4198a603cb21b25dc7ed0a76
David Ebert <ebertek@gmail.com>
2020-11-19 23:32:39 +0100

    Create db.php

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+  $host    = 'localhost';
+  $db      = 'gyilkosos';
+  $user    = 'gyilkosos';
+  $pass    = 'Who has the lighter?';
+  $charset = 'utf8mb4';
+  $dsn     = "mysql:host=$host;dbname=$db;charset=$charset";
+  $options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_PERSISTENT => false
+  ];
+  try {
+    $dbh = new PDO($dsn, $user, $pass, $options);
+  } catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+  }
+  $sth = $dbh->query('SELECT uid FROM users');
+  while ($row = $sth->fetch()) {
+    echo $row['uid'] . "\n";
+  }
+  $dbh = null;
+?>
